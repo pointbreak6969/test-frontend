ec9e118d1a458ee8a2a53db6bfe5368cd9f4fa0f
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-06-22 19:51:59 +0545

    Refactor appwriteConfig.js for improved code organization and readability

--- src/appwrite/appwriteConfig.js
+++ src/appwrite/appwriteConfig.js
@@ -12,6 +12,49 @@ export class Service {
       this.databases = new Databases(this.client);
       this.bucket = new Storage(this.client);
   }
+  async createDocument(collectionId, data) {
+    try {
+      return await this.databases.createDocument(collectionId, data);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getDocuments(collectionId) {
+    try {
+      return await this.databases.listDocuments(collectionId);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(file);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getFiles() {
+    try {
+      return await this.bucket.listFiles();
+    } catch (error) {
+      throw error;
+    }
+  }
+  async createTask(data) {
+    try {
+      return await this.queue.createTask(data);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getTasks() {
+    try {
+      return await this.queue.listTasks();
+    } catch (error) {
+      throw error;
+    }
+  }
+  
 }
 const service = new Service();
 export default service;
